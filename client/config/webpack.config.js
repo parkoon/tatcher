23870757d7cc73b7b2df57a2819597c0b054a4b0
parwhdgur <jh.park@qualson.com>
2020-10-05 11:06:37 +0900

    fix: alias path 🛠

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -277,8 +277,8 @@ module.exports = function (webpackEnv) {
                 .map((ext) => `.${ext}`)
                 .filter((ext) => useTypeScript || !ext.includes('ts')),
             alias: {
-                '@': path.resolve(__dirname, 'src'),
-                '@Components': path.resolve(__dirname, 'src/components'),
+                '@': path.resolve(__dirname, '../src'),
+                '@Components': path.resolve(__dirname, '../src/components'),
                 // Support React Native Web
                 // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
                 'react-native': 'react-native-web',
