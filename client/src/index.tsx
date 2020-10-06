e5ef298148e1806d21103a00c1c714adb639542e
parwhdgur <jh.park@qualson.com>
2020-10-06 14:35:03 +0900

    feat: add reset css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
 import App from './App'
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+`
 
 ReactDOM.render(
     <React.StrictMode>
+        <GlobalStyle />
         <App />
     </React.StrictMode>,
     document.getElementById('root'),
