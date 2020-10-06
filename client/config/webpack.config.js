ca8c49b9a4d3978406e929db3cbc85fc544fe573
parwhdgur <jh.park@qualson.com>
2020-10-06 14:34:49 +0900

    feat: add alias

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -279,6 +279,7 @@ module.exports = function (webpackEnv) {
             alias: {
                 '@': path.resolve(__dirname, '../src'),
                 '@Components': path.resolve(__dirname, '../src/components'),
+                '@Modals': path.resolve(__dirname, '../src/modals'),
                 // Support React Native Web
                 // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
                 'react-native': 'react-native-web',
