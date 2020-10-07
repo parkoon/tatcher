c9655fdd84796ca05690081bd7b33ed68a68f3c1
parkoon <devparkoon@gmail.com>
2020-10-07 10:10:20 +0900

    feat: add click to icon button

--- client/src/components/IconButton/IconButton.tsx
+++ client/src/components/IconButton/IconButton.tsx
@@ -4,9 +4,10 @@ import { Wrapper } from './styles'
 
 type IconButtonProps = {
     icon: React.ReactNode
+    onClick(): void
 }
-function IconButton({ icon }: IconButtonProps) {
-    return <Wrapper>{icon}</Wrapper>
+function IconButton({ icon, ...props }: IconButtonProps) {
+    return <Wrapper {...props}> {icon}</Wrapper>
 }
 
 export default IconButton
