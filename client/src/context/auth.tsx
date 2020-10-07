c411e16462c02063b0dce2a97b4c2b58c15b265b
parkoon <devparkoon@gmail.com>
2020-10-07 14:05:13 +0900

    feat: add auth context

---
+++ client/src/context/auth.tsx
@@ -0,0 +1,88 @@
+import React, { createContext, useReducer, useContext } from 'react'
+import jwtDecode from 'jwt-decode'
+
+const initialState = {
+    user: null,
+}
+
+if (localStorage.getItem('jwt')) {
+    const jwt = localStorage.getItem('tuid')
+
+    const decodedToken = jwt ? jwtDecode<any>(jwt) : null
+
+    if (decodedToken.exp * 1000 < Date.now()) {
+        localStorage.removeItem('jwt')
+    } else {
+        initialState.user = decodedToken
+    }
+}
+
+type AuthContextType = {
+    login(user: User): void
+    logout(): void
+    user: User | null
+}
+export const AuthContext = createContext({} as AuthContextType)
+
+export type User = { token: string }
+
+type State = {
+    user: User | null
+}
+type Action = { type: 'LOGIN'; payload: User } | { type: 'LOGOUT' }
+
+function authReducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'LOGIN':
+            return {
+                ...state,
+                user: action.payload,
+            }
+        case 'LOGOUT':
+            return {
+                ...state,
+                user: null,
+            }
+        default:
+            return state
+    }
+}
+
+type AuthProviderProps = {
+    children: React.ReactNode
+}
+export function AuthProvider({ children }: AuthProviderProps) {
+    const [state, dispatch] = useReducer(authReducer, initialState)
+
+    const login = (user: User) => {
+        localStorage.setItem('tuid', user.token)
+        dispatch({
+            type: 'LOGIN',
+            payload: user,
+        })
+    }
+
+    const logout = () => {
+        localStorage.removeItem('tuid')
+        dispatch({
+            type: 'LOGOUT',
+        })
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{
+                login,
+                logout,
+                user: state.user,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext)
+    return context
+}
