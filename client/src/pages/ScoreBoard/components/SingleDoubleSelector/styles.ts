032a503f6e35eec73731fa41793009c31e0231f4
parkoon <devparkoon@gmail.com>
2020-10-10 15:15:05 +0900

    chore: change margin value

--- client/src/pages/ScoreBoard/components/SingleDoubleSelector/styles.ts
+++ client/src/pages/ScoreBoard/components/SingleDoubleSelector/styles.ts
@@ -3,12 +3,13 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
 
     margin-top: 72px;
 
     div {
         &:first-child {
-            margin-right: 12px;
+            margin-right: ${({ theme }) => theme.margins.base};
         }
     }
 `
