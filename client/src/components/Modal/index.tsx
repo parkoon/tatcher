4556f8531071ba0ffc7e911f98e1ab08f77282be
parkoon <devparkoon@gmail.com>
2020-11-17 23:40:07 +0900

    feat: show / hide state

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 type ModalProps = {
     children: React.ReactNode
 }
 function Modal({ children }: ModalProps) {
-    return (
-        <div className={'modal-wrapper'}>
-            <div className={'modal-backdrop'} />
-            <div className={'modal-box'}>
-        {children}
+
+    const [display, setDisplay] = useState(true)
+
+    const open = () => {
+        setDisplay(true)
+    }
+
+    const close = () => {
+        setDisplay(false)
+    }
+
+    if (display) {
+        return (
+            <div className={'modal-wrapper'}>
+                <div className={'modal-backdrop'} onClick={close} />
+                <div className={'modal-box'}>
+            {children}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+    return null
+
 }
 
 export default Modal
